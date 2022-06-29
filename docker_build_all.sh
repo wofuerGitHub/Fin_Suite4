a4f04b2fbdb439528f1b1d9049a55782209cd6ae
wofuerGitHub <wofuer@gmail.com>
2022-06-30 00:27:20 +0200

    installation

--- docker_build_all.sh
+++ docker_build_all.sh
@@ -6,8 +6,14 @@ docker build --pull --rm -f "Dockerfile_update_company_key_stats" -t update_comp
 docker build --pull --rm -f "Dockerfile_update_income_statement" -t update_income_statement:latest "."
 docker build --pull --rm -f "Dockerfile_update_symbols_list" -t update_symbols_list:latest "."
 
-docker build --pull --rm -f "Dockerfile_update_fx_eur" -t update_fx_eur:latest "."
+docker build --pull --rm -f "Dockerfile_update_fx_stream" -t update_fx_stream:latest "."
+docker build --pull --rm -f "Dockerfile_update_quote_stream" -t update_quote_stream:latest "."
+
+# docker build --pull --rm -f "Dockerfile_update_fx_eur" -t update_fx_eur:latest "."
 
 docker-compose -f "docker-compose.yml" up -d --build --remove-orphans
 
-docker-compose -f "docker-compose_update_fx_eur.yml" up -d --build
+# docker-compose -f "docker-compose_update_fx_eur.yml" up -d --build
+
+docker-compose -f "docker-compose_update_fx_stream.yml" up -d --build
+docker-compose -f "docker-compose_update_quote_stream.yml" up -d --build
